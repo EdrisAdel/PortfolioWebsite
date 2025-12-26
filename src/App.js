46aada7334ec0a83c3613173faece6384b3a84ce
Edris <edrisadel64@gmail.com>
2025-12-25 23:47:31 -0500

    canonical tag fix

--- src/App.js
+++ src/App.js
@@ -4,14 +4,44 @@ import About from './components/About';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 import './App.css';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useLocation } from 'react-router-dom';
 import { useEffect } from 'react';
 
 
 function App() {
+  const location = useLocation();
+
   useEffect(() => {
     document.title = "Edris's Portfolio";
-  }, []);
+
+    const baseUrl = 'https://edrisadel.dev';
+    let canonicalPath = '/';
+
+    switch (location.pathname) {
+      case '/about':
+        canonicalPath = '/about';
+        break;
+      case '/projects':
+        canonicalPath = '/projects';
+        break;
+      case '/contact':
+        canonicalPath = '/contact';
+        break;
+      default:
+        canonicalPath = '/';
+        break;
+    }
+
+    const canonicalUrl = `${baseUrl}${canonicalPath}`;
+
+    let link = document.querySelector("link[rel='canonical']");
+    if (!link) {
+      link = document.createElement('link');
+      link.setAttribute('rel', 'canonical');
+      document.head.appendChild(link);
+    }
+    link.setAttribute('href', canonicalUrl);
+  }, [location.pathname]);
   return (
     <>
       <Routes>
